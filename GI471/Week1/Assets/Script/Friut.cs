609dab11904a60de4fe435b60d39e5504c25261c
DESKTOP-K890P5J\xAnnieNC <Kaittipong.jitk@bumail.net>
2020-09-05 21:28:02 +0700

    GI471_HomeworkWeek1 1610700948

---
+++ GI471/Week1/Assets/Script/Friut.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Friut : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text text;
+    public Text text1, text2, text3, text4;
+    
+    List<string> fruit = new List<string>();
+    
+    
+    
+    private void Start()
+    {
+        
+        fruit.Add("Apple");
+        fruit.Add("Orange");
+        fruit.Add("Carrot");
+        fruit.Add("Strawberry");
+        fruit.Add("Banana");
+        fruit.Add("");
+        
+
+
+
+
+    }
+    void Update()
+    {
+        
+
+       
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+            fruit.RemoveAt(0);
+
+
+            
+
+            if (text4.text == "Banana")
+            {
+                text4.text = null;
+            }
+            else if (text3.text == "Banana")
+            {
+                text3.text = "";
+            }
+
+            else if (text2.text == "Banana")
+            {
+                text2.text = "";
+            }
+            else if (text1.text == "Banana")
+            {
+                text1.text = "";
+            }
+            else if (text.text == "Banana")
+            {
+                text.text = "";
+           
+               
+            }
+            else if(text.text=="")
+            {
+                text.text ="Item is Empty";
+            }
+ 
+        }
+        text.text = fruit[0];
+        text1.text = fruit[1];
+        text2.text = fruit[2];
+        text3.text = fruit[3];
+        text4.text = fruit[4];
+
+    }
+   
+}
---
+++ GI471/Week1/Assets/Script/ImagePopUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ImagePopUp : MonoBehaviour
+{
+    public Sprite apple,orange,carrot, strawberry, banana;
+  
+
+    private void Update()
+    {
+
+       
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == strawberry)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = banana;
+                this.gameObject.GetComponent<Image>().sprite = banana;
+            }
+            else if (this.gameObject.GetComponent<SpriteRenderer>().sprite == carrot)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = strawberry;
+                this.gameObject.GetComponent<Image>().sprite = strawberry;
+            }
+            else if(this.gameObject.GetComponent<SpriteRenderer>().sprite == orange)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = carrot;
+                this.gameObject.GetComponent<Image>().sprite = carrot;
+            }
+            else if (this.gameObject.GetComponent<SpriteRenderer>().sprite == apple)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = orange;
+                this.gameObject.GetComponent<Image>().sprite = orange;
+            }
+
+            
+            else if (this.gameObject.GetComponent<SpriteRenderer>().sprite == null)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = apple;
+                this.gameObject.GetComponent<Image>().sprite = apple;
+
+            }
+        }
+    }
+}
