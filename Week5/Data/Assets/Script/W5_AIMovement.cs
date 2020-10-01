ca83451e98d6f07db40c108dbe3466e9ab49b697
DESKTOP-K890P5J\xAnnieNC <Kaittipong.jitk@bumail.net>
2020-10-01 23:50:11 +0700

    Week5

---
+++ Week5/Data/Assets/Script/W5_AIMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class W5_AIMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private NavMeshAgent navAgent;
+    private W5_PlayerMovement playerTarget;
+    private void Start()
+    {
+        navAgent = this.GetComponent<NavMeshAgent>();
+
+    }
+
+    private void Update()
+    {
+        FindingPlayer();
+        UpdateMovement();
+
+
+    }
+    private void UpdateMovement()
+    {
+        if (playerTarget == null)
+            return;
+
+            navAgent.SetDestination(playerTarget.transform.position);
+    }
+    private void FindingPlayer()
+    {
+        if(playerTarget==null)
+        {
+            playerTarget = FindObjectOfType<W5_PlayerMovement>();
+        }
+
+    }
+}
---
+++ Week5/Data/Assets/Script/W5_PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W5_PlayerController : MonoBehaviour
+{
+    public W5_PlayerMovement playerMovement;
+
+    public void Update()
+    {
+        playerMovement.Setinput_Horizontal(Input.GetAxis("Horizontal"));
+        playerMovement.Setinput_Vertical(Input.GetAxis("Vertical"));
+        playerMovement.SetInput_Jump(Input.GetKeyDown(KeyCode.Space));
+        
+    }
+}
